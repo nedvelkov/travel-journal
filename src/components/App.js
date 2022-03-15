e47545aa7dad4ae522811f8545ec5ac5be7c7798
nedvelkov <69073418+nedvelkov@users.noreply.github.com>
2022-03-15 15:31:15 +0200

    finished travel-journal app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import Navbar from "./Navbar";
+import Card from "./Card";
+import data from "../data";
 
 function App() {
+  var cards = data.map((x) => {
+    return <Card {...x} />;
+  });
   return (
     <div>
-      <Navbar/>
-      App
+      <Navbar />
+      {cards}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import pin from "../icons/pin.png";
 
 function Card(props) {
   return (
-    <div>Card</div>
-  )
+    <>
+      <div className="card-container">
+        <img src={props.imageUrl} alt={props.title} />
+        <div className="card-info">
+          <div>
+            <img src={pin} alt="pin" />
+            <span className="card-location">{props.location}</span>
+            <a href={props.googleMapsUrl} className="mapsAnchor">
+              View on Google Maps
+            </a>
+          </div>
+          <h1 className="card-title">{props.title}</h1>
+          <h3 className="card-dates">
+            {props.startDate}-{props.endDate}
+          </h3>
+          <p className="card-description">{props.description}</p>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
 }
 
-export default Card
+export default Card;
